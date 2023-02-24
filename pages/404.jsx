eee957abef92ee23bb5ee41a50047e6771e7c8db
Miro Manestar <miroimanestar@gmail.com>
2023-02-24 00:40:01 -0500

    Fixed 404 url

--- pages/404.jsx
+++ pages/404.jsx
@@ -12,7 +12,7 @@ const NotFound = () => {
                                 <h1 className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
                                 <p className="mt-2 text-base text-gray-400">Sorry, I couldn&apos;t find the page you&apos;re looking for.</p>
                                 <div className="mt-6">
-                                    <a href="#" className="text-base font-medium duration-200 text-highlight hover:text-gray-200">
+                                    <a href="/" className="text-base font-medium duration-200 text-highlight hover:text-gray-200">
                                         Go back home
                                         <span aria-hidden="true"> &rarr;</span>
                                     </a>

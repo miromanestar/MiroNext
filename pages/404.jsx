08faa0ca707b6bf0d7af82261f2abd3682a50139
Miro Manestar <miroimanestar@gmail.com>
2023-02-24 17:22:29 -0500

    Fixed 404 link elem

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import Container from "../components/Container"
 
 const NotFound = () => {
@@ -12,10 +13,10 @@ const NotFound = () => {
                                 <h1 className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
                                 <p className="mt-2 text-base text-gray-400">Sorry, I couldn&apos;t find the page you&apos;re looking for.</p>
                                 <div className="mt-6">
-                                    <a href="/" className="text-base font-medium duration-200 text-highlight hover:text-gray-200">
+                                    <Link href="/" className="text-base font-medium duration-200 text-highlight hover:text-gray-200">
                                         Go back home
                                         <span aria-hidden="true"> &rarr;</span>
-                                    </a>
+                                    </Link>
                                 </div>
                             </div>
                         </div>

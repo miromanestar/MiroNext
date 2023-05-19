d47cd6ab49645c9e90fa39c7d22f1d082c04b20e
Miro Manestar <miroimanestar@gmail.com>
2023-05-19 13:04:22 -0400

    Slight word changes

--- pages/projects/hawaiiprb.jsx
+++ pages/projects/hawaiiprb.jsx
@@ -94,11 +94,9 @@ const HawaiiPRB = () => (
         </div>
 
         <p className=" sm:text-lg text-gray-300 mb-4 mt-8">
-            Because three dimensions were selected, three-dimensional plots using the R rgl package was created for each region. A full window plot for Hawaii can be
+            Because three dimensions were selected, three-dimensional plots were generated. A full window plot for Hawaii can be
             accessed <Link href="/plots/hawaii" className="text-highlight duration-100 hover:text-highlight2">here</Link> and the plot for the 
             PRB can be accessed <Link href="/plots/prb" className="text-highlight duration-100 hover:text-highlight2">here</Link>. 
-            Please note that the coloring will not be the same as the plots shown above. However, if one were to line up the axes of the 3D plots
-            with its corresponding 2D plot, the distribution of data would be the same.
         </p>
 
         <div className="flex gap-3 flex-col md:flex-row">

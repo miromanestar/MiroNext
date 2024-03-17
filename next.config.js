9db5604f40b42423b3a806f420dae5a91223603f
Miro Manestar <miroimanestar@gmail.com>
2024-03-17 02:50:21 -0400

    Added WASM snake

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
-    config.experiments = { asyncWebAssembly: true }
-    return config
-  }
+  // webpack(config) {
+  //   config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+  //   config.experiments = { asyncWebAssembly: true }
+  //   return config
+  // }
 }
 
 module.exports = nextConfig
